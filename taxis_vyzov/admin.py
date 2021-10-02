613460d892e4608e4a2a24de5478f929c53c37b8
dyyura <anarayaq@gmail.com>
2021-10-02 21:14:21 +0600

    chize

--- taxis_vyzov/admin.py
+++ taxis_vyzov/admin.py
@@ -1,7 +1,6 @@
-# from django.contrib import admin
-# from .models import *
-#
-# admin.site.register(Order)
-#
-# admin.site.register(Order)
-#
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Order)
+
+
