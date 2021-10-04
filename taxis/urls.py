2a32a6f71bff6455886d1d348ff0977548ff5b26
ramil27 <gahramanov_ramil@icloud.com>
2021-10-04 22:03:12 +0600

    ok

--- taxis/urls.py
+++ taxis/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
@@ -21,7 +22,7 @@ from taxis_vyzov import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('register', views.register, name='register')
+    url(r'^$', views.home, name='home'),
+    url('', views.register, name='register')
 ]
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
